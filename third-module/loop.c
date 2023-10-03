2ac664e081f02abb635a5acc47065b712c3a0d41
sajib925 <sajibahmed1004@gmail.com>
2023-10-04 01:04:02 +0600

    module three

---
+++ third-module/loop.c
@@ -0,0 +1,261 @@
+// c programming have 3 types: 1.for loop. 2.while loop. 3.do while loop
+// for loop: for(initialization, condition, increament or decreament) {whatever i want loop}
+
+// increament for loop
+#include<stdio.h>
+
+// int main () {
+//     int i;
+//     for (i=0;i <= 10 ; i++) {
+//     printf("hello world\n");
+//     }
+//     return 0;
+// }
+
+// decreament for loop
+// #include<stdio.h>
+
+// int main () {
+//     int i;
+//     for (i=10;i >= 0 ; i--) {
+//     printf("hello world\n");
+//     }
+//     return 0;
+// }
+
+
+// #include<stdio.h>
+
+// int main () {
+//     int i;
+//     for (i=0;i <= 10 ; i++) {
+//     printf("%d\n",i);
+//     }
+//     return 0;
+// }
+
+// #include<stdio.h>
+
+// int main () {
+//     int i;
+//     for (i=10;i >= 0 ; i--) {
+//     printf("%d\n",i);
+//     }
+//     return 0;
+// }
+
+// 1 to 10 jogfol ber korar niyom
+// #include<stdio.h>
+
+// int main () {
+//     int sum= 0;
+//     int i;
+//     for (i=0;i <= 10 ; i++) {
+//         sum+=i;
+//     };
+
+//     printf("%d\n",sum);
+//     return 0;
+// }
+
+// 1 to je kono sonkhar jogfol ber korar niyom
+// #include<stdio.h>
+
+// int main () {
+//     long long int sum= 0;
+//     int i;
+//     int n;
+//     scanf("%d", &n);
+//     for (i=1;i <= n ; i++) {
+//         sum+=i;
+//     };
+
+//     printf("%lld\n",sum);
+//     return 0;
+// }
+
+// 1 to je kono even sonkhar jogfol ber korar niyom
+// #include<stdio.h>
+
+// int main () {
+//     int i;
+//     for (i=1;i <= 10 ; i++) {
+//         if (i%2==0)
+//         {
+//            printf("%d\n  even",i);
+//         } 
+//         else {
+//             printf("%d\n  odd",i);
+//         }
+        
+//     };
+
+    
+//     return 0;
+// }
+
+// how to get even number
+// #include<stdio.h>
+
+// int main () {
+//     int i;
+//     for (i=1;i <= 10 ; i++) {
+//         if (i%2==0)
+//         {
+//            printf("%d\n",i);
+//         } 
+//     };
+    
+//     return 0;
+// }
+
+// // how to get odd number
+// #include<stdio.h>
+
+// int main () {
+//     int i;
+//     for (i=1;i <= 10 ; i++) {
+//         if (i%2 != 0)
+//         {
+//            printf("%d\n",i);
+//         } 
+//     };
+    
+//     return 0;
+// }
+
+// Break statement
+// #include<stdio.h>
+
+// int main () {
+//     int i;
+//     for (i=1;i <= 10 ; i++) {
+//         if (i == 5)
+//         {
+//            break;
+//         } 
+//         printf("%d\n",i);
+//     };
+    
+//     return 0;
+// }
+// Break statement ei vabe likhle 5 soho print hobe but upore 5 er ag porjonto hobe
+// #include<stdio.h>
+
+// int main () {
+//     int i;
+//     for (i=1;i <= 10 ; i++) {
+//          printf("%d\n",i);
+//         if (i == 5)
+//         {
+//            break;
+//         } 
+       
+//     };
+    
+//     return 0;
+// }
+
+// continue statement
+// #include<stdio.h>
+
+// int main () {
+//     int i;
+//     for (i=1;i <= 10 ; i++) {
+//         if (i == 5)
+//         {
+//            continue;;
+//         } 
+//         printf("%d\n",i);
+//     };
+    
+//     return 0;
+// }
+
+// while loop example
+
+// #include<stdio.h>
+
+// int main () {
+//     int i = 1;
+
+//     while (i <= 10 ) {
+//         printf("%d\n",i);
+//         i++;
+//     };
+    
+//     return 0;
+// }
+
+// do while loop example
+
+// #include<stdio.h>
+
+// int main () {
+//     int i = 1;
+    
+//     do {
+//         printf("%d\n",i);
+//         i++;
+//     } while (i <= 20 );
+    
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+// int main()
+// {
+//     int k;
+//     for (k = -3; k < -5; k++)
+//         printf("Hello ");
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main()
+// {
+//     int i = 0;
+//     while (i < 3) {
+//         i++;
+//     }
+//    printf("%d",i);
+//    return 0;
+// }
+
+
+// #include <stdio.h>
+// int main()
+// {
+//     int a = 0, i;
+//     for (i = 0;i < 5; i++)
+//     {
+//         a++;
+//         continue;
+//     }
+//     printf("%d %d",i,a);
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main()
+// {
+//     int a = 0, i;
+//     for (i = 0;i < 5; i++)
+//     {
+//         continue;
+//         a++;
+//     }
+//     printf("%d %d",i,a);
+//     return 0;
+// }
+
+#include <stdio.h>
+int main()
+{
+    int i = 0;
+    while (i = 0) {
+        printf("Hello\n");
+    }
+    return 0;
+}
